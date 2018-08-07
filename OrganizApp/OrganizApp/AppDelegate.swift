73ade73f73fa27dfa5a17cec28b6cc31c92aa132
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2018-08-07 13:40:08 -0700

    Modified Logo design and

--- OrganizApp/OrganizApp/AppDelegate.swift
+++ OrganizApp/OrganizApp/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import CoreData
 import Firebase
+import UserNotifications
 
 @UIApplicationMain
 
@@ -26,6 +27,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let initialViewController = UIStoryboard.initialViewController(for: .home)
         window?.rootViewController = initialViewController
         window?.makeKeyAndVisible()
+        
+//        registerForPushNotifications()
+        
         
         return true
     }
@@ -117,6 +121,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UD.defaults.set(orgDict, forKey: "organizationsDict")
         UD.defaults.set(orgArray, forKey: "organizationsArray")
     }
+    
+    
+    
+    //Register push notifications and all that
+    
+//    func registerForPushNotifications() {
+//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
+//            (granted, error) in
+//            print("Permission granted: \(granted)")
+//
+//            guard granted else { return }
+//            self.getNotificationSettings()
+//        }
+//    }
+//
+//    func getNotificationSettings() {
+//        UNUserNotificationCenter.current().getNotificationSettings { (settings) in
+//            print("Notification settings: \(settings)")
+//            guard settings.authorizationStatus == .authorized else { return }
+//            UIApplication.shared.registerForRemoteNotifications()
+//        }
+//    }
+//
+//    func application(_ application: UIApplication,
+//                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        let tokenParts = deviceToken.map { data -> String in
+//            return String(format: "%02.2hhx", data)
+//        }
+//
+//        let token = tokenParts.joined()
+//        print("Device Token: \(token)")
+//    }
+//
+//    func application(_ application: UIApplication,
+//                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+//        print("Failed to register: \(error)")
+//    }
 
 }
 
