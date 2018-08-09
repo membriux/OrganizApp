39439d0ab20edb3de4f635fb3be1f964b3b98e2a
Guillermo Ignacio Sanchez Lamas <sanguillermo98@gmail.com>
2018-08-08 17:29:12 -0700

    Completed basic tutorial functionality

--- OrganizApp/OrganizApp/Controllers/TutorialViewController.swift
+++ OrganizApp/OrganizApp/Controllers/TutorialViewController.swift
@@ -19,9 +19,51 @@ class TutorialViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        errorLabel.text = ""
     }
     
     @IBAction func finishButtonTapped(_ sender: UIButton) {
+        finished()
+    }
+    
+    
+    @IBAction func addOrganization(_ sender: UIButton) {
+        guard let targetOrg = inputTextField.text,
+            !targetOrg.isEmpty else {return}
+        
+        // Add organization to userDefault array if found
+        OrganizationService.find(targetOrg: targetOrg) { (organization) in
+            if organization.count != 0 {
+                let org = organization[0].organizationUsername
+                let orgId = organization[0].uid
+                
+                // Checks if organizaiton was saved before
+                if UD.orgsDict[org] == nil {
+                    
+                    // Save organization
+                    Organization.save(org, orgId)
+                    UD.currentOrgId = orgId
+                    UD.defaults.set(orgId, forKey: "currentOrgId")
+                    
+                    UD.currentOrg = org
+                    UD.defaults.set(org, forKey: "currentOrg")
+                    self.finished()
+                }
+                
+            } else {
+                self.showError(org: targetOrg)
+            }
+        }
+        
+    }
+    
+    func showError(org: String) {
+        errorLabel.text = "\(org) not found. Check spelling."
+        
+        
+    }
+    
+    func finished() {
         let initialViewController: UIViewController
         initialViewController = UIStoryboard.initialViewController(for: .home)
         self.view.window?.rootViewController = initialViewController
@@ -29,11 +71,10 @@ class TutorialViewController: UIViewController {
     }
     
     
-    @IBAction func addOrganization(_ sender: UIButton) {
-        
-        
-        
-    }
+    
+    
+    
+    
     
     
     
